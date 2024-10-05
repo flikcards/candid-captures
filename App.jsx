1704bb07de6e0bf25592f196da1ed217c82204f1
flikcards <168372038+flikcards@users.noreply.github.com>
2024-10-05 11:57:39 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Work from "./pages/Work";
+
+function App() {
+
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
